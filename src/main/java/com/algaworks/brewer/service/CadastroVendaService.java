3f8172a0a34ba44db2e1ba59a283becb9ca50038
Adriano <adriano.psa@gmail.com>
2017-07-03 23:31:55 -0300

    First Commit

---
+++ src/main/java/com/algaworks/brewer/service/CadastroVendaService.java
@@ -0,0 +1,20 @@
+package com.algaworks.brewer.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.algaworks.brewer.model.Venda;
+import com.algaworks.brewer.repository.Vendas;
+
+@Service
+public class CadastroVendaService {
+	
+	@Autowired
+	private Vendas vendas;
+	
+	@Transactional
+	public void salvar(Venda venda) {
+		vendas.save(venda);
+	}
+}
